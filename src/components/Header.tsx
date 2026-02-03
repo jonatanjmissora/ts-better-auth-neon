266a1074b3af051001e7c657a45d2133c06233c2
Jonatan <jonatanjmissora@gmail.com>
2026-02-03 20:36:43 -0300

    colocando theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,28 @@
 import { authClient } from "lib/auth-client";
 import { Button } from "./ui/button";
 import { Link, useNavigate } from "@tanstack/react-router";
+import { Moon, Sun } from "lucide-react";
+import { useState } from "react";
 
 export default function Header() {
 
   const {data: session} = authClient.useSession();
   const navigate = useNavigate();
+
+const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+    const toggleTheme = () => {
+        if(typeof window !== 'undefined') {
+            const html = document.documentElement;
+            if(html.classList.contains('dark')) {
+                html.classList.remove('dark');
+                setTheme('light');
+            } else {
+                html.classList.add('dark');
+                setTheme('dark');
+            }
+        }
+    };
 
   const logout = async () => {
     await authClient.signOut({
@@ -19,9 +36,16 @@ export default function Header() {
   };
 
   return (
-    <header className="p-4 px-20 bg-gray-800 text-white shadow-lg">
+    <header className="p-4 px-20">
       <nav className="flex items-center justify-between">
-        <span>Logo</span>
+        <span className="text-xl font-semibold">Logo</span>
+        <div className="flex items-center gap-4">
+        <button className="" onClick={toggleTheme}>
+            {
+                theme === 'dark' ? <Sun size={20} /> : <Moon size={20} />
+            }
+        </button>
+
           {
             session 
             ? (
@@ -37,6 +61,7 @@ export default function Header() {
             </div>
             )
           }
+        </div>
       </nav>
     </header>
   )
